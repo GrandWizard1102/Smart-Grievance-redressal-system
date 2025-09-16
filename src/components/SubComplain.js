359e689bcde4ee590eecbd5495633d27913efef8
GrandWizard1102 <v.kavimani1106@gmail.com>
2025-09-16 19:45:48 +0530

    Updated components and styles

--- src/components/SubComplain.js
+++ src/components/SubComplain.js
@@ -4,17 +4,32 @@ const SubComplain=()=>{
         <>
         <h1 style={{textAlign:"center",color:"darkslategray"}}>Submit complaint</h1>
         <div id='subcol'>
-           <div>
+           <div id="grid-item">
             <h3>Recent community Issues</h3>
             </div>
-            <form id="compdet">
-                <h3>Issue details</h3>
-                <h4>Issue title*</h4>
-                <input type="text" required></input>
-
-            </form>
-            <div>
-
+            <div id="complaintinput">
+                <form id="compdet">
+                    <h3>Issue details</h3>
+                    <h4>Issue title*</h4>
+                    <input id="title" type="text" required placeholder='Brief title'></input>
+                    <h4 for="category">Select a Category*</h4>
+                    <select id="category" required placeholder='select category'>
+                        <option value="" selected disabled>-- Select a fruit --</option>
+                        <option value="Water Supply">Water Supply</option>
+                        <option value="Electricity">Electricity</option>
+                        <option value="Roads">Roads</option>
+                    </select>
+                    <h4>Location</h4>
+                    <input id="location" type="text" required placeholder='Enter Location'></input>
+                </form>
+                <form>
+                    <h3>Description</h3>
+                    <textarea id="message" type="text" name="message" rows="5" cols="40" placeholder='Brief description here' />
+                </form>
+            </div>
+            <div id="grid-item">
+            <h3 for="img">Photo Upload</h3>
+            <input type="file" id="img" name="img" accept='image/*' required/>
             </div>
         </div>
         </>
