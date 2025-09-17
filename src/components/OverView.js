c2ae8cd1c808c5d2f10406a66a3dbfdb54daa9dd
naveen <naveenkumar03202006@gmail.com>
2025-09-17 07:54:38 +0530

    completed

--- src/components/OverView.js
+++ src/components/OverView.js
@@ -1,4 +1,5 @@
-import { IssueBox } from './issuebox';
+
+import { Issuesview } from './issueboxfull';
 import OverHeader from './OverHeader'
 
 const OverView=({user})=>{
@@ -6,7 +7,7 @@ const OverView=({user})=>{
     return(
         <>
         <OverHeader title={user} />
-        <IssueBox label="Pothole on Main Street" discription="There is a large pothole on Main Street that is causing traffic issues." catogory="Road Issue" status="Pending" priority="High" date="2023-10-01" location="Main Street, Cityville" />
+        <Issuesview/>
         </>
 
     )
