17975fe2945859cc1ae8afd70bc34fab7254ae65
naveen <naveenkumar03202006@gmail.com>
2025-09-18 13:29:53 +0530

    completed

--- src/components/heading.jsx
+++ src/components/heading.jsx
@@ -4,8 +4,8 @@ import DownloadIcon from '@mui/icons-material/Download';
 import RefreshIcon from '@mui/icons-material/Refresh';
 export function Header({title,subtitle,showicon}) {
     return(
-        <Card elevation={0} sx={{borderRadius:3,display:"flex",justifyContent:"space-between",alignItems:"center",padding:"10px",mb:2}}>
-            <Stack  direction="row" spacing={1}   sx={{alignItems:"center",justifyContent:"center",padding:"10px"}} >
+        <Card elevation={1} sx={{borderRadius:3,display:"flex",justifyContent:"space-between",alignItems:"center",padding:"10px",mb:2}}>
+            <Stack  direction="row" spacing={1}   sx={{alignItems:"center",justifyContent:"center",padding:"10px",paddingLeft:"30px"}} >
                 <Stack alignItems="center" justifyContent="center" sx={{backgroundColor:"#43ca80",color:"white",borderRadius:"40%",width:50,height:50}}>
                     <DescriptionIcon fontSize="large" />
                 </Stack>
