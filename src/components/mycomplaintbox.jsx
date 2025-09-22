910ee6f9b0c95d7c4f03d463e28a9e3b16c4bf06
naveen <naveenkumar03202006@gmail.com>
2025-09-22 07:47:22 +0530

    improement on my complaint section

--- src/components/mycomplaintbox.jsx
+++ src/components/mycomplaintbox.jsx
@@ -1,4 +1,6 @@
-import { Box, Button, Chip, Paper, Stack, Typography } from "@mui/material";
+import { Box, Button, Chip, Dialog, IconButton, Paper, Stack, Typography,DialogTitle,
+  DialogContent,
+  DialogActions, } from "@mui/material";
 import RoomOutlinedIcon from '@mui/icons-material/RoomOutlined';
 import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
 import DoneOutlineIcon from '@mui/icons-material/DoneOutline';
@@ -9,7 +11,9 @@ import ReportProblemOutlinedIcon from '@mui/icons-material/ReportProblemOutlined
 import Color from './Color'
 import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
 import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
+import { useState } from "react";
 export function Mycomplainbox({label, discription, catogory, status,priority,date,location }) {
+    const [pop,setpop]=useState(false);
     const getcolor=(status)=>{
         switch(status?.toLowerCase()){
             case "pending": return "warning";
@@ -65,10 +69,17 @@ export function Mycomplainbox({label, discription, catogory, status,priority,dat
                 <Stack >
                     <Typography variant="body1" sx={{ padding: "5px" }}>{discription}</Typography>
                 </Stack>
-                <Box sx={{ display: "flex", justifyContent: "space-between" }} >
+                <Stack>
+                    <Stack spacing={0.5} direction="column" sx={{backgroundColor:"#b2d1ffff",borderRadius:"10px",padding:"5px",paddingLeft:"10px"}}>
+                    <Typography variant="body2"  fontWeight="bold">Management Response:</Typography>
+                    <Typography varianr="caption" >we have arranged a team to sort out the problem</Typography>
+                    </Stack>
+                </Stack>
+
+                <Box sx={{ display: "flex", justifyContent: "space-between",padding:"5px" }} >
                     <Chip label={status} color={getcolor(status)} variant="filled"  sx={{ textDecoration: "none" }}/>
                     <Stack direction="row" spacing={0.5} alignItems="center" >
-                        <Button variant="outlined" size="medium" sx={{borderRadius:3,textTransform: "none"}} startIcon={<RemoveRedEyeOutlinedIcon/>} color="info" >Veiw details</Button>
+                        <Button variant="outlined" size="medium" sx={{borderRadius:3,textTransform: "none"}} startIcon={<RemoveRedEyeOutlinedIcon/>} color="info" onClick={()=>setpop(true)}>Veiw details</Button>
                         <Button variant="outlined" size="medium" sx={{borderRadius:3,textTransform: "none"}} startIcon={<CancelOutlinedIcon/>} color="error" >cancel</Button>
                     </Stack>
                 </Box>
@@ -76,6 +87,24 @@ export function Mycomplainbox({label, discription, catogory, status,priority,dat
 
 
             </Paper>
+            <Dialog open={pop} onClose={()=>setpop(false)} fullWidth maxWidth="sm">
+            
+                    <Stack direction="row" justifyContent="space-between" alignItems="center">
+                        <Typography variant="h6" fontWeight="bold">{label}</Typography>
+                        <IconButton onClick={()=>setpop(false)}>
+                            <CancelOutlinedIcon/>
+                        </IconButton>
+                    </Stack>
+
+               
+                
+                    <Stack spacing={2}>
+                        <Stack direction="row" spacing={1} alignItems="center" >  
+                        </Stack> 
+                        </Stack>
+                
+
+            </Dialog>
         </>
-    )
+    );
 }
